268800b4b356ccdd9e15407b8c4cc78d7d75a305
leilafaez <leila.faez@gmail.com>
2024-02-24 13:31:21 +0000

    implement preview not available for part1 of task

--- tasks/conditional-rendering/src/profilePreview.js
+++ tasks/conditional-rendering/src/profilePreview.js
@@ -1,22 +1,30 @@
 export function ProfilePreview(previewInfo) {
   const preview = document.createElement("aside");
+  if (previewInfo.available && previewInfo.profileInfo) {
+    
 
-  const picture = document.createElement("img");
-  picture.src = previewInfo.profileInfo.pictureSrc;
-  picture.alt = "";
-  picture.dataset.testid = "profilePicture";
+    const picture = document.createElement("img");
+    picture.src = previewInfo.profileInfo.pictureSrc;
+    picture.alt = "";
+    picture.dataset.testid = "profilePicture";
 
-  const name = document.createElement("p");
-  name.textContent = previewInfo.profileInfo.name;
-  name.dataset.testid = "profileName";
+    const name = document.createElement("p");
+    name.textContent = previewInfo.profileInfo.name;
+    name.dataset.testid = "profileName";
 
-  const bio = document.createElement("p");
-  bio.textContent = previewInfo.profileInfo.bio;
-  bio.dataset.testid = "profileBio";
+    const bio = document.createElement("p");
+    bio.textContent = previewInfo.profileInfo.bio;
+    bio.dataset.testid = "profileBio";
 
-  preview.appendChild(picture);
-  preview.appendChild(name);
-  preview.appendChild(bio);
+    preview.appendChild(picture);
+    preview.appendChild(name);
+    preview.appendChild(bio);
 
-  return preview;
+    return preview;
+  } else {
+    const available = document.createElement("p");
+    available.textContent = "Profile preview unavailable.";
+    preview.appendChild(available);
+    return preview;
+  }
 }
